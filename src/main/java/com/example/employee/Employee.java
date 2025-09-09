139b2c4036118ea046274c07148babeb35f5754b
qiulu2 <luca.qiu@oocl.com>
2025-09-09 15:09:40 +0800

    feat: post /employees to creat employee

---
+++ src/main/java/com/example/employee/Employee.java
@@ -0,0 +1,5 @@
+package com.example.employee;
+
+public record Employee(Integer id, String name, int age, String gender, double salary) {
+
+}
---
+++ src/test/java/com/example/employee/EmployeeControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.employee;
+
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_return_create_employee_when_post() throws Exception {
+        String requestBody = """
+                {
+                    "name": "John Smith",
+                    "age": 32,
+                    "gender": "Male",
+                    "salary": 5000.0
+                }
+                """;
+
+        MockHttpServletRequestBuilder request = post("/employees").contentType(MediaType.APPLICATION_JSON).content(requestBody);
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("John Smith"))
+                .andExpect(jsonPath("$.age").value(32))
+                .andExpect(jsonPath("$.gender").value("Male"))
+                .andExpect(jsonPath("$.salary").value(5000.0));
+
+    }
+
+}
